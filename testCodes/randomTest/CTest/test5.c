3c7f11979bf0ae3b58745a751f360100adc149cc
jeffrey <jeffrey@Taiwinner.com>
2022-12-12 10:39:30 +0800

    modified test5.c

---
+++ testCodes/randomTest/CTest/test5.c
@@ -0,0 +1,14 @@
+// This test is for testing the pointer properties of array.
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+//	int testArray_len = 3;
+	int testArray[3] = {1, 2, 3};
+
+	// Show the testArray result.
+	for(int i = 1; i <= 2; ++i){
+		printf("%d\n", testArray[i]);
+	}
+	
+}
