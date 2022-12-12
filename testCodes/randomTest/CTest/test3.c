c719c787ea31979b444c216231c7829cacf6550c
jeffrey <jeffrey@Taiwinner.com>
2022-12-12 10:24:13 +0800

    add test3.c to repo

---
+++ testCodes/randomTest/CTest/test3.c
@@ -0,0 +1,10 @@
+// Test for string libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void main(){
+	char testStr[] = "this is a test.";
+	printf("%s\n", testStr);
+	printf("%d\n", strlen(testStr));
+}
