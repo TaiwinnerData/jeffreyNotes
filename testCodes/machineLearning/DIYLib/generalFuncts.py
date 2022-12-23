8fb31ccb455366acf6ffb5a091411ed2694efc60
jeffrey <jeffrey@Taiwinner.com>
2022-12-23 11:37:57 +0800

    modified generalFuncts.py

--- testCodes/machineLearning/DIYLib/generalFuncts.py
+++ testCodes/machineLearning/DIYLib/generalFuncts.py
@@ -4,12 +4,10 @@ y = [2, 4, 6, 8]
 
 # test function y = wx + b and the distance square is (wx_test + b - y_test)^2 + (x - x_test)^2
 
-def least_action_funct(x, y):
-    w = 1
-    b = 1
-    for i in range(len(x)):
-
-
+#def least_action_funct(x, y):
+#    w = 1
+#    b = 1
+#    for i in range(len(x)):
 
 
 # create differential 
@@ -25,16 +23,16 @@ def s_diff(funct, x):
     return dy/dx
 
 
-def newton_method(funct):
+def newton_method(funct, times):
     x = 30
-    for i in range(100):
+    for i in range(times):
         x = x -funct(x)/f_diff(funct, x)
     return x
 
 
-def newton_method_extreme(funct):
+def newton_method_extreme(funct, times):
     x = 30
-    for i in range(100):
+    for i in range(times):
         x = x - f_diff(funct, x)/s_diff(funct, x)
     return x
 
@@ -53,11 +51,11 @@ def main():
     print("Show second order differential reulst of test_function")
     print(s_diff(test_funct, 3))
     print("Show newtom method to find the solution of the test_function")
-    print(newton_method(test_funct))
+    print(newton_method(test_funct, 100))
     print("Show newton method to find the extreme value of test_function")
-    print(newton_method_extreme(test_funct))
+    print(newton_method_extreme(test_funct, 100))
     print("Show newton method to find the extreme value of test_function2")
-    print(newton_method_extreme(test_funct2))
+    print(newton_method_extreme(test_funct2, 100))
 
 
 
