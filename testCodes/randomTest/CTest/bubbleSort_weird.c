17f157a731670a64b8259c9e7e66e3b725fe62b2
jeffrey <jeffrey@Taiwinner.com>
2023-01-10 12:49:12 +0800

    add bubbleSort_weird.c to repo

---
+++ testCodes/randomTest/CTest/bubbleSort_weird.c
@@ -0,0 +1,28 @@
+// This script is for testing bubble sort method.
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int *bubbleSort(int (* testArray)[10]){
+	int len_of_array = sizeof(*testArray)/sizeof(int);
+	for(int i=0; i<len_of_array; ++i){
+		for(int j=0; j<len_of_array-i-1; ++j){
+			if((*testArray)[j] > (*testArray)[j+1]){
+				int temp_value = (*testArray)[j];
+				(*testArray)[j] = (*testArray)[j+1];
+				(*testArray)[j+1] = temp_value;
+			}
+		}
+	}
+	return testArray;
+}
+
+
+void main(){
+	int testArray[10] = {5, 1, 7, 2, 3, 7, 22, 1, 78, 11};
+	bubbleSort(&testArray);
+	// show testArray
+	for(int i=0; i<10; ++i){
+		printf("testArray[%d]: %d\n", i, testArray[i]);
+	}
+}
