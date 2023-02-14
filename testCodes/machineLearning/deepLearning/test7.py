755dd6357d09b632645222edadd976f895e4d8da
jeffrey <jeffrey@Taiwinner.com>
2023-02-14 12:29:58 +0800

    modified test7.py

--- testCodes/machineLearning/deepLearning/test7.py
+++ testCodes/machineLearning/deepLearning/test7.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-
+import random as random
 
 
 # Create neural network object class by using backward propagation.
@@ -8,27 +8,19 @@ class NN(object):
     def __init__(self):
         self.feature_number = 2  # feature numbers
         self.output_number = 1 # class number
-        self.hidden_number = 2 # only single layer with 6 units
+        self.hidden_number = 3 # only single layer with 6 units
 
         # initilization matrix of weights In this case we have 2 layers of weights
         np.random.seed(1)
         self.w1_2 = np.random.randn(self.feature_number, self.hidden_number) # first layer's weights  2*2 matrix
-        print("self.w1_2")
-        print(self.w1_2)
         self.w2_3 = np.random.randn(self.hidden_number, self.output_number) # second layer's weights  2*1 matrix
-        print("self.w2_3")
-        print(self.w2_3)
 
     def _forward_propagation(self, x_input): # 
         # layer 1
         self.net1 = x_input 
         self.net2 = np.dot(self.net1, self.w1_2)
         self.net2 = np.float64(self.net2) # change the data type to numpy's float or the _simgoid can't read it.
-        print("show self.net2")
-        print(self.net2)
         self.o2 = self._sigmoid(self.net2)
-        print("show self.o2")
-        print(self.o2)
         
         # layer 2
         self.net3 = np.dot(self.o2, self.w2_3)
@@ -51,36 +43,39 @@ class NN(object):
         self.w1_2_grad = np.dot(self.net1.T, self.delta2)
         self.w2_3_grad = np.dot(self.net2.T, self.delta3)
 
-    def _update(self):
-        self.w1_2 = self.w1_2 + self.w1_2_grad
-        self.w2_3 = self.w2_3 + self.w2_3_grad
+    def _update(self, learning_rate=0.01):
+#        self.w1_2 = self.w1_2 - np.multiply(learning_rate, self.w1_2_grad)
+#        self.w2_3 = self.w2_3 - np.multiply(learning_rate, self.w2_3_grad)
+        self.w1_2 = self.w1_2 - learning_rate*self.w1_2_grad
+        self.w2_3 = self.w2_3 - learning_rate*self.w2_3_grad
 
     def train(self, X, y, iteration):
         for i in range(iteration):
-            self._forward_propagation(X[i])
-            self._backward_propagation(y[i])
+            self._forward_propagation(X)
+            self._backward_propagation(y)
             self._update()
 
 
-
-
+#    def predict(self, X):
+#        y_pred = self.
 
 
 # import example data, for this test I use weight-height datasets.
 # input the datasets
 datasets = pd.read_csv("weight-height.csv")
 datasets = pd.DataFrame.to_numpy(datasets)
+np.random.shuffle(datasets)
 
-# define the train data
-train_number = 200 
+# define train data
+train_number = 2
 train_X, train_y = datasets[:train_number, 1:3], datasets[:train_number, 0:1]
+
 # define the test data
 test_number = 200
 test_X, test_y = datasets[train_number:train_number+test_number, 1:3], datasets[train_number:train_number+test_number, 0:1]
 
-
 # turn all the y value to 1 or 0
-for i in range(len(train_number)):
+for i in range(train_number):
     if train_y[i] == "Male":
         train_y[i] = 1
     else:
@@ -89,8 +84,24 @@ for i in range(len(train_number)):
 
 def main():
     testNN = NN()
-    testNN._forward_propagation(datasets)
-    testNN._backward_propagation(data_result)
+#    testNN._forward_propagation(train_X)
+#    testNN._backward_propagation(train_y)
+
+    testNN.train(train_X, train_y, 30)
+
+    testData = np.array([64, 145])
+    testNN._forward_propagation(testData)
+    print("weight: " + str(testData[0]) + "height: " + str(testData[1]))
+    if testNN.o3 > 0.5:
+        print(testNN.o3)
+        print(1)
+        print("male")
+    else:
+        print(testNN.o3)
+        print(0)
+        print("female")
+
+
 
 
 
