497a1e16c1210002454d78821e12cd81e5794d45
jeffrey <jeffrey@Taiwinner.com>
2023-03-27 15:11:35 +0800

    modified runBackend.py

--- testCodes/machineLearning/deepLearning/digit_recognization/userBackend/runBackend.py
+++ testCodes/machineLearning/deepLearning/digit_recognization/userBackend/runBackend.py
@@ -8,6 +8,7 @@ from PIL import Image
 from io import BytesIO
 import cv2
 import json
+import digit_recognization4 as dig_reco
 
 
 # Backend test codes
@@ -37,24 +38,45 @@ def get_image():
     image_np = np.array(image)
     img = image_np
 
+    print(image_np)
+    print(img)
 
-    if img.shape[2] == 4:
-        # Create a mask of the transparent pixels
-        alpha_mask = img[:, :, 3] == 0
 
-        # Replace the transparent pixels with white
-        img[alpha_mask] = [255, 255, 255, 255]
+#    if img.shape[2] == 4:
+#        # Create a mask of the transparent pixels
+#        alpha_mask = img[:, :, 3] == 0
+#
+#        # Replace the transparent pixels with white
+#        img[alpha_mask] = [255, 255, 255, 255]
+#    inv_img = cv2.bitwise_not(img)
+    img = img[:, :,3:4]
+    print("image after mask")
+    print(img)
     # invert the color
     inv_img = cv2.bitwise_not(img)
+    inv_img = cv2.bitwise_not(inv_img)
+    print("inv_img")
+    print(inv_img)
     # convert the image to grayscale
-    gray_img = cv2.cvtColor(inv_img, cv2.COLOR_BGR2GRAY)
-    result_img = gray_img
+#    gray_img = cv2.cvtColor(inv_img, cv2.COLOR_BGR2GRAY)
+#    gray_img = cv2.cvtColor(inv_img, cv2.COLOR_RGBA2RGB)
+#    result_img = gray_img
+    result_img = inv_img
+    print("show result image")
+    print(result_img)
+
+
+    # load the machine learning model
+    testNN = dig_reco.NN()
+    testNN.load_trainMD()
+    result_img = result_img.flatten()
+    result_img = result_img/5
+    test_X = np.array([result_img])
+    predict_result = testNN.predict(test_X)
+    print(predict_result)
 
 
 
-
-    print("image_np")
-    print(image_np)
 
 
     result = {'img': 'test'}
